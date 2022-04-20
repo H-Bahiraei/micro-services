cf8dbc5b838b5ec684df3c237ce78df29240228d
Mahdi Sharifi <picher_s@yahoo.com>
2022-04-20 15:19:57 +0430

    Change config server

--- gatewayserver/src/main/java/ir/bki/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/ir/bki/gatewayserver/GatewayserverApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class GatewayserverApplication {
-
+     // Impirtant
     public static void main(String[] args) {
         SpringApplication.run(GatewayserverApplication.class, args);
     }
